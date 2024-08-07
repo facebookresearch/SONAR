322914cdb4c8ac9ce858a7c0e23b11e7cd8334fc
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-08-07 11:15:51 +0100

    Move TexToEmbedding config params from abstract

--- huggingface_pipelines/text.py
+++ huggingface_pipelines/text.py
@@ -1,14 +1,28 @@
-from sonar.inference_pipelines.text import EmbeddingToTextModelPipeline
+from sonar.inference_pipelines.text import TextToEmbeddingModelPipeline, EmbeddingToTextModelPipeline
 import logging
 from typing import List, Dict, Any
 from .pipeline import Pipeline, PipelineOverwrites, PipelineConfig
-from sonar.inference_pipelines.text import TextToEmbeddingModelPipeline
 import torch
 from dataclasses import dataclass, replace
 import numpy as np
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+class TextToEmbeddingOverwrites(PipelineOverwrites, total=False):
+
+    """
+        Overwrite options for TexToEmbeddingPipeline configuration.
+
+
+        Attributes:
+            max_seq_len (int): Number of a tokens per sequence. Defaults to None which means maximum for model.
+    """
+
+    max_seq_len: int
+    encoder_model: str
+    source_lang: str
 
 
 class EmbeddingToTextOverwrites(PipelineOverwrites, total=False):
@@ -37,7 +51,11 @@ class TextToEmbeddingPipelineConfig(PipelineConfig):
         device (str): The device to use for computation (e.g., 'cpu' or 'cuda').
     """
 
-    def with_overwrites(self, overwrites: PipelineOverwrites) -> 'TextToEmbeddingPipelineConfig':
+    max_seq_len: int = None
+    encoder_model: str = "text_sonar_basic_encoder"
+    source_lang: str = "eng_Latn"
+
+    def with_overwrites(self, overwrites: EmbeddingToTextOverwrites) -> 'TextToEmbeddingPipelineConfig':
         """
         Create a new configuration with the specified overwrites.
 
@@ -79,48 +97,28 @@ class EmbeddingToTextPipelineConfig(PipelineConfig):
         return replace(self, **overwrites)
 
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 class HFEmbeddingToTextPipeline(Pipeline):
-    """
-    Pipeline for converting embeddings back to text using a Hugging Face model.
-
-    Attributes:
-        config (EmbeddingToTextPipelineConfig): Configuration for the pipeline.
-        t2t_model (EmbeddingToTextModelPipeline): The model used for decoding embeddings to text.
-    """
-
     def __init__(self, config: EmbeddingToTextPipelineConfig):
-        """
-        Initialize the embedding-to-text model after the instance is created.
-        """
         logger.info("Initializing embedding to text model...")
+        self.config = config
         self.t2t_model = EmbeddingToTextModelPipeline(
             decoder=self.config.decoder_model,
             tokenizer=self.config.decoder_model,
             device=self.config.device
         )
-
-        self.config = config
         logger.info("Model initialized.")
 
     def process_batch(self, batch: Dict[str, Any]) -> Dict[str, List[str]]:
-        """
-        Process a batch of embeddings and convert them back to text.
-
-        Args:
-            batch (Dict[str, Any]): A dictionary containing the batch data with embeddings.
-
-        Returns:
-            Dict[str, List[str]]: The input batch with additional columns containing the decoded texts.
-
-        Raises:
-            AssertionError: If the input columns don't contain lists of embeddings.
-        """
         for column in self.config.columns:
             embeddings = batch[column]
             assert all(isinstance(item, list) for item in embeddings), \
                 f"Column {column} must contain only lists of embeddings, not individual embeddings."
-
-            all_embeddings = [embed for item in embeddings for embed in item]
+            all_embeddings = np.vstack([np.array(embed)
+                                       for item in embeddings for embed in item])
             all_decoded_texts = self.decode_embeddings(all_embeddings)
 
             reconstructed_texts = []
@@ -134,34 +132,17 @@ class HFEmbeddingToTextPipeline(Pipeline):
             batch[f"{column}_{self.config.output_column_suffix}"] = reconstructed_texts
             logger.info(
                 f" {column} column reconstructed:  {batch[column][:5]}")
-
         return batch
 
-    def decode_embeddings(self, embeddings: List[Any]) -> List[str]:
-        """
-        Decode a list of embeddings into text.
-
-        Args:
-            embeddings (List[Any]): A list of embeddings to decode.
-
-        Returns:
-            List[str]: A list of decoded texts corresponding to the input embeddings.
-
-        Raises:
-            Exception: If there's an error during the decoding process.
-        """
+    def decode_embeddings(self, embeddings: np.ndarray) -> List[str]:
         try:
             logger.info(f"Decoding {len(embeddings)} embeddings...")
-            embeddings = [torch.tensor(embed) if not isinstance(
-                embed, torch.Tensor) else embed for embed in embeddings]
-            embeddings = [embed.unsqueeze(0) if embed.dim(
-            ) == 1 else embed for embed in embeddings]
-            batched_embeddings = torch.cat(embeddings, dim=0)
+            embeddings_tensor = torch.from_numpy(embeddings).float()
 
             decoded_texts = []
             for i in range(0, len(embeddings), self.config.batch_size):
-                batch_embeddings = batched_embeddings[i:i +
-                                                      self.config.batch_size]
+                batch_embeddings = embeddings_tensor[i:i +
+                                                     self.config.batch_size]
                 batch_decoded = self.t2t_model.predict(
                     batch_embeddings,
                     target_lang=self.config.target_lang,
@@ -178,45 +159,20 @@ class HFEmbeddingToTextPipeline(Pipeline):
 
 
 class HFTextToEmbeddingPipeline(Pipeline):
-    """
-    Pipeline for converting text to embeddings using a Hugging Face model.
-
-    Attributes:
-        config (TextToEmbeddingPipelineConfig): Configuration for the pipeline.
-        t2vec_model (TextToEmbeddingModelPipeline): The model used for encoding text to embeddings.
-    """
-
     def __init__(self, config: TextToEmbeddingPipelineConfig):
-        """
-        Initialize the text-to-embedding model after the instance is created.
-        """
         logger.info("Initializing text to embedding model...")
+        self.config = config
         self.t2vec_model = TextToEmbeddingModelPipeline(
             encoder=self.config.encoder_model,
             tokenizer=self.config.encoder_model,
             device=self.config.device
         )
 
-        self.config = config
-
     def process_batch(self, batch: Dict[str, Any]) -> Dict[str, Any]:
-        """
-        Process a batch of texts and convert them to embeddings.
-
-        Args:
-            batch (Dict[str, Any]): A dictionary containing the batch data with text columns.
-
-        Returns:
-            Dict[str, Any]: The input batch with additional columns containing the computed embeddings.
-
-        Raises:
-            AssertionError: If the input columns don't contain lists of sentences.
-        """
         for column in self.config.columns:
             if column in batch:
                 assert all(isinstance(item, list) for item in batch[column]), \
                     f"Column {column} must contain only lists of sentences, not individual strings."
-
                 all_sentences = [sentence for item in batch[column]
                                  for sentence in item]
                 all_embeddings = self.encode_texts(all_sentences)
@@ -234,30 +190,20 @@ class HFTextToEmbeddingPipeline(Pipeline):
                     f" {column} column embeddings:  {batch[column][:5]}")
             else:
                 logger.warning(f"Column {column} not found in batch.")
-
         return batch
 
-    def encode_texts(self, texts: List[str]) -> List[np.ndarray]:
-        """
-        Encode a list of texts into embeddings.
-
-        Args:
-            texts (List[str]): A list of texts to encode.
-
-        Returns:
-            List[np.ndarray]: A list of embeddings corresponding to the input texts.
-
-        Raises:
-            Exception: If there's an error during the encoding process.
-        """
+    def encode_texts(self, texts: List[str]) -> np.ndarray:
         try:
             embeddings = []
             for i in range(0, len(texts), self.config.batch_size):
                 batch_texts = texts[i:i + self.config.batch_size]
                 batch_embeddings = self.t2vec_model.predict(
-                    batch_texts, source_lang=self.config.source_lang, batch_size=self.config.batch_size)
+                    batch_texts, source_lang=self.config.source_lang, batch_size=self.config.batch_size, max_seq_len=self.config.max_seq_len)
+
+                batch_embeddings = [emb.cpu().numpy()
+                                    for emb in batch_embeddings]
                 embeddings.extend(batch_embeddings)
-            return embeddings
+            return np.array(embeddings)
         except Exception as e:
             logger.error(f"Error encoding texts or sentences: {e}")
             raise
